bf0950257b4463aef97520c4c66cdd9aec7dfc50
yzhou463 <43074553+yzhou463@users.noreply.github.com>
2019-07-28 22:07:06 -0400

    code

---
+++ code/test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> division = new ArrayList<>(Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1));
+        System.out.println(division);
+
+    }
+}
